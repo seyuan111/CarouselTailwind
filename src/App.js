f62da658e8dcca06a691631a778e4a804b4aeafb
Sean Yuan <ysean54@yahoo.com>
2023-04-10 20:57:06 -0400

    Carousel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {data} from './mockData'
+import {AiOutlineArrowLeft, AiOutlineArrowRight} from 'react-icons/ai'
 
 function App() {
+
+  const slideLeft = () => {
+    const slider = document.getElementById('slider')
+    slider.scrollLeft = slider.scrollLeft - 500
+  }
+
+  const slideRight = () => {
+    const slider = document.getElementById('slider')
+    slider.scrollLeft = slider.scrollLeft + 500
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className="w-full h-[260px]" src="https://sportsbetting.legal/wp-content/uploads/2023/03/NBA-PLAYOFF-BRACKET-based-on-the-standings-for-3-6-23.jpg"></img>
+      <div className="relative flex items-center">
+      <AiOutlineArrowLeft className="cursor-pointer" size={30} onClick={slideLeft}/>
+      <div id="slider" className="w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide">
+      {data.map((item) => (
+        <img className="w-[130px] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300" src={item.img}></img>
+      ))}
+      </div>
+      <AiOutlineArrowRight className="cursor-pointer" size={30} onClick={slideRight}/>
+      </div>
     </div>
   );
 }
---
+++ src/mockData.js
@@ -0,0 +1,50 @@
+export const data = [
+    {
+        id: 1,
+        img: 'https://upload.wikimedia.org/wikipedia/en/thumb/2/25/New_York_Knicks_logo.svg/800px-New_York_Knicks_logo.svg.png'
+    },
+    {
+        id: 2,
+        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Brooklyn_Nets_newlogo.svg/1200px-Brooklyn_Nets_newlogo.svg.png'
+    },
+    {
+        id: 3,
+        img: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4a/Milwaukee_Bucks_logo.svg/1200px-Milwaukee_Bucks_logo.svg.png'
+    },
+    {
+        id: 4,
+        img: 'https://b.fssta.com/uploads/application/nba/team-logos/Cavaliers.png'
+    },
+    {
+        id: 5,
+        img: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/8f/Boston_Celtics.svg/1200px-Boston_Celtics.svg.png'
+    },
+    {
+        id: 6,
+        img: 'https://upload.wikimedia.org/wikipedia/en/thumb/f/f1/Memphis_Grizzlies.svg/1200px-Memphis_Grizzlies.svg.png'
+    },
+    {
+        id: 7,
+        img: 'https://b.fssta.com/uploads/application/nba/team-logos/Nuggets.vresize.350.350.medium.0.png'
+    },
+    {
+        id: 8,
+        img: 'https://s.yimg.com/it/api/res/1.2/gKyPp665PsPlIuWx5UK2WQ--~A/YXBwaWQ9eW5ld3M7dz0xMjAwO2g9NjMwO3E9MTAw/https://s.yimg.com/cv/apiv2/default/nba/20181218/500x500/clippers_wbg.png'
+    },
+    {
+        id: 9,
+        img: 'https://upload.wikimedia.org/wikipedia/en/thumb/c/c7/SacramentoKings.svg/1200px-SacramentoKings.svg.png'
+    },
+    {
+        id: 10,
+        img: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/0e/Philadelphia_76ers_logo.svg/640px-Philadelphia_76ers_logo.svg.png'
+    },
+    {
+        id: 11,
+        img: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/dc/Phoenix_Suns_logo.svg/1200px-Phoenix_Suns_logo.svg.png'
+    },
+    {
+        id: 12,
+        img: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/01/Golden_State_Warriors_logo.svg/1200px-Golden_State_Warriors_logo.svg.png'
+    }
+]
